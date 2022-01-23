ead31f3b7d64f07c362d18c69e25c07477304d76
Suraj Vijay <surajcpp115@gmail.com>
2022-01-23 20:51:22 +0530

    uploading/deleting images to/from cloudinary

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const { createServer } = require("http");
 const express = require("express");
 const { Server } = require("socket.io");
+
+// Custom
+const multerUploads = require("./utils/multer.utils");
+const convertToDataURI = require("./utils/to-uri.utils");
+const { cloudinary } = require("./utils/cloudinary.utils");
 
 const app = express();
 const httpServer = createServer(app);
@@ -8,6 +14,28 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 app.use(express.json());
+
+app.post("/images/upload", multerUploads, async (req, res) => {
+    try {
+        const imageUri = convertToDataURI(req.file);
+        const uploadResult = await cloudinary.uploader.upload(imageUri, {
+            folder: process.env.PRESET
+        });
+        return res.status(200).send(uploadResult);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+app.delete("/images/delete", async (req, res) => {
+    try {
+        const publicId = req.body.imageURL.split('/').slice(-2).join('/').split('.')[0];
+        const deletedResponse = await cloudinary.uploader.destroy(publicId);
+        return res.status(200).json(deletedResponse);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
 
 io.on("connection", socket => {
     console.log(`[SERVER] ${socket.id} Connected`);
---
+++ utils/multer.utils.js
@@ -0,0 +1,6 @@
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const multerUploads = multer({ storage }).single('userImage');
+
+module.exports = multerUploads;
